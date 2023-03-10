38bc3f2c6e17843855266eb7cc210ce62ef33015
mbuimbogo <mbuimbogo99@gmail.com>
2023-03-10 10:03:54 +0300

    Add the favicon svg

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,12 @@ import './globals.css'
 export const metadata = {
   title: 'Google clone app',
   description: 'Google clone created by nextjs 13',
+  icons: {
+    icon: '/favicon.svg',
+  },
 }
+
+
 
 export default function RootLayout({ children }) {
   return (
