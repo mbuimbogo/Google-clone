30ec93dc651390372a10a89aa62df6a95fdd88f5
mbuimbogo <mbuimbogo99@gmail.com>
2023-03-15 02:51:08 +0300

    add search total results and search time

---
+++ src/components/WebSearchResults.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function WebSearchResults({results}) {
+  return (
+    <div>
+      <p>
+        About {results.searchInformation?.formattedTotalResults} results ({results.searchInformation?.formattedSearchTime} seconds)
+      </p>
+    </div>
+  )
+}
