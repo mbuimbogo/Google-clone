dbaadb9524b35914ff1380e78b908a5d9c3fe4bb
mbuimbogo <mbuimbogo99@gmail.com>
2023-03-15 03:54:04 +0300

    fetch image results and create the ImageSearchResults component

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,9 +1,29 @@
+import ImageSearchResults from '@/components/ImageSearchResults'
+import Link from 'next/link'
 import React from 'react'
 
-export default function ImageSeachPage() {
-  return (
-    <div>
-      oee
+export default async function ImageSearchPage({searchParams}) {
+
+  await new Promise((resolve)=> setTimeout(resolve,10000))
+  
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=Image`) //get the API & API_KEY & API_CONTEXT from google developers rest API programable search Engine
+
+  if(!response.ok){
+    throw new Error("Something went wrong")
+  }
+  const data = await response.json()
+  
+  const results = data.items
+
+  if(!results){
+    return <div className="flex flex-col justify-center items-center">
+      <h1 className='text-3xl mb-4'>Pia wewe kuwa serious</h1>
+      <p className='text-lg'>Try searching for something or go back to the homepage{" "}
+      <Link className='text-blue-500' href="/">Home</Link>
+      </p>
     </div>
-  )
+    
+  }
+  return<>{results && <ImageSearchResults results={data}/>}</>
+  
 }
