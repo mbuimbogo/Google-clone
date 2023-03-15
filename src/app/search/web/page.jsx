aaf27427cd0173b8736c63976dfa951f8a0a5556
mbuimbogo <mbuimbogo99@gmail.com>
2023-03-15 03:39:35 +0300

    setTimeout for the promise to 10seconds to prevent exhaustion of API key and render WebSearchResults page

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,8 +1,11 @@
+import WebSearchResults from '@/components/WebSearchResults'
 import Link from 'next/link'
 import React from 'react'
 
 export default async function WebSearchPage({searchParams}) {
 
+  await new Promise((resolve)=> setTimeout(resolve,10000))
+  
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`) //get the API & API_KEY & API_CONTEXT from google developers rest API programable search Engine
 
   if(!response.ok){
@@ -20,6 +23,6 @@ export default async function WebSearchPage({searchParams}) {
     </div>
     
   }
-  return<>{results && results.map((result) => <h1>{result.title}</h1>)}</>
+  return<>{results && <WebSearchResults results={data}/>}</>
   
 }
