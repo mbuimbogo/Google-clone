7cbe17e291b4e72a1bc2561a906c648c1d83f3c0
mbuimbogo <mbuimbogo99@gmail.com>
2023-03-15 04:33:19 +0300

    add my name to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,8 @@ export default function Footer() {
     <div>
       <footer className='absolute bottom-0 text-sm text-gray-500 bg-[#f2f2f2] w-full'>
         <div className="border-b px-8 py-3">
-        <CountryLookup/>
+        <CountryLookup />
+        
         </div>
         <div className="flex flex-col sm:flex-row justify-between items-center px-8 py-3 space-y-7 sm:space-y-0">
             <ul className='flex items-center space-x-6'>
@@ -15,7 +16,7 @@ export default function Footer() {
                 <li className="link">Business</li>
                 <li className="link">How Search works</li>
             </ul>
-
+            <p>Made with ❤️ by mbuimbogo</p>
             <ul className='flex items-center space-x-6'>
                 <li className="link">Privacy</li>
                 <li className="link">Terms</li>
